c81c7a61db963a11d370572776f0a50e3e5da725
Niklas Terei <terei@match.uni-hannover.de>
2024-11-06 16:20:24 +0100

    skills added

---
+++ pm_skills_2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'pm_skills_2'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='mll',
+    maintainer_email='terei@match.uni-hannover.de',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'pm_skills_2 = pm_skills_2.pm_skills_2:main'
+        ],
+    },
+)
